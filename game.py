cf8fd23ac3474139703dd4c1df14b59938f87523
Elizabeth McCall <emccall137@gmail.com>
2022-02-11 20:21:16 -0500

    capitalized Gold

--- game.py
+++ game.py
@@ -22,7 +22,7 @@ class Crossbow(Weapon):
 
 def play():
     print("Welcome to Dungeon Trek!")
-    inventory = [Dagger(), "gold", "bread"]
+    inventory = [Dagger(), "Gold", "bread"]
     while True:
         action_input = get_player_command()
         if action_input in ['n', 'N', 'north']:
