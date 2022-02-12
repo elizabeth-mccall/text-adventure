2e97336865bb0a8c7a82becaead5cb85285614c2
Elizabeth McCall <emccall137@gmail.com>
2022-02-11 20:34:00 -0500

    deleted morefood

--- morefood.py
+++
@@ -1,6 +0,0 @@
-   class Food:
-       def __init__(self, name, carbs, protein, fat):
-           self.name = name
-           self.carbs = carbs
-           self.protein = protein
-           self.fat = fat
