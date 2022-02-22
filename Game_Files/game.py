e6a9ca20523e18fdd93ee0baa759b75c831e3e7c
Elizabeth McCall <emccall137@gmail.com>
2022-02-22 12:59:00 -0500

    refactoring (print available actions each turn)

--- Game_Files/game.py
+++ Game_Files/game.py
@@ -1,6 +1,45 @@
+from ast import Or
+from collections import OrderedDict
+
 from player import Player
 import world
 
+def get_available_actions(room, player):
+    actions = OrderedDict()
+    print("Choose an action: ")
+    if player.inventory:
+        action_adder(actions, 'i', player.print_inventory, "Print inventory")
+    if isinstance(room, world.EnemyTile) and room.enemy.is_alive():
+        action_adder(actions, 'a', player.attack, "Attack")
+    else:
+        if world.tile_at(room.x, room.y - 1):
+            action_adder(actions, 'n', player.move_north, "Go north")
+        if world.tile_at(room.x, room.y + 1):
+            action_adder(actions, 's', player.move_south, "Go south")
+        if world.tile_at(room.x + 1, room.y):
+            action_adder(actions, 'e', player.move_east, "Go east")
+        if world.tile_at(room.x - 1, room.y):
+            action_adder(actions, 'w', player.move_west, "Go west")
+    if player.hp < 100:
+        action_adder(actions, 'h', player.heal, "Heal")
+    return actions
+
+def action_adder(action_dict, hotkey, action, name):
+    action_dict[hotkey.lower()] = action
+    action_dict[hotkey.upper()] = action
+    print("{}: {}".format(hotkey, name))
+
+def choose_action(room, player):
+    action = None
+    while not action:
+        available_actions = get_available_actions(room, player)
+        action_input = input("Action: ")
+        action = available_actions.get(action_input)
+        if action:
+            action()
+        else:
+            print("Invalid action!")
+ 
 def play():
     print("Welcome to Dungeon Trek!")
     player = Player()
@@ -8,26 +47,6 @@ def play():
         room = world.tile_at(player.x, player.y)
         print(room.intro_text())
         room.modify_player(player)
-        action_input = get_player_command()
-        if action_input in ['n', 'N', 'north']:
-            player.move_north()
-        elif action_input in ['s', 'S', 'south']:
-            player.move_south()
-        elif action_input in ['e', 'E', 'east']:
-            player.move_east()
-        elif action_input in ['w', 'W', 'west']:
-            player.move_west()
-        elif action_input in ['i', 'I', 'inventory']:
-            player.print_inventory()
-        elif action_input in ['a', 'A', 'attack']:
-            player.attack()
-        elif action_input in ['h', 'H', 'heal']:
-            player.heal()
-        else:
-            print("Invalid action!")
-
-
-def get_player_command():
-    return input("Action: ")
+        choose_action(room, player)
 
 play()
